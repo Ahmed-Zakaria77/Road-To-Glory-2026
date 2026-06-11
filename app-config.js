dea08d69e7eaa8b6b7ddb5e7fc46f05587c6d81d
Mustafa Ahmed <mustafa.softcode@gmail.com>
2026-06-11 03:16:09 +0300

    Fix Firebase config

--- app-config.js
+++ app-config.js
@@ -1,10 +1,7 @@
 window.WC2026_CONFIG = {
   remoteStorage: {
-    // Use Firebase Realtime Database with GitHub Pages.
-    // Example:
-    firebaseDatabaseUrl: "https://road-to-glory-2026-default-rtdb.firebaseio.com/",
     provider: "firebase-rest",
-    firebaseDatabaseUrl: "",
+    firebaseDatabaseUrl: "https://road-to-glory-2026-default-rtdb.firebaseio.com/",
     firebaseAuth: "",
     firebaseStatePath: "wc2026/shared-state"
   }
